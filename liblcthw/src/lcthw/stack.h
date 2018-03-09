01e3c61d4a976d03907f2f92c30b143fdfff08fc
t00361654 <tanzhuxiaqiu@huawei.com>
2018-03-09 17:26:13 +0800

    add stack

---
+++ liblcthw/src/lcthw/stack.h
@@ -0,0 +1,21 @@
+#ifndef lcthw_Stack_h
+#define lcthw_Stack_h
+#include <lcthw/list.h>
+
+#define Stack List
+
+#define Stack_create() List_create()
+
+#define Stack_destroy(S) List_destory((S))
+
+#define Stakc_count(S) List_count((S))
+
+#define Stack_push(S, V) List_push((S), (V))
+
+#define Stack_pop(S) List_pop((S))
+
+#define Stack_peek(S) List_last((S))
+
+#define STACK_FOREACH(S, C) LIST_FOREACH((S), first, next, (C))
+
+#endif
